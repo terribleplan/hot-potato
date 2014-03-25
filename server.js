08687df8464282fa8337f5dcc821b5bc0c109128
terribleplan <kegan@keganmyers.com>
2014-03-24 22:16:11 -0500

    Bencode, beer, etc.

--- server.js
+++ server.js
@@ -1,125 +1,124 @@
+var bencode = require('bencode');
+
 var LISTEN_PORT = 1407;
 var net = require('net');
 var LOCAL_NETWORK_PREFIX = '192.168.88.';
 
 // allow one connection for each machine on the same local network 192.168.88.x
 var connections_by_octet = (function(){
-  var rv = [];
-  for(var i = 0; i !== 256; ++i) rv.push(null);
-  return rv;
+    var rv = [];
+    for(var i = 0; i !== 256; ++i) rv.push(null);
+    return rv;
 })();
 
 // function for sending the potato to a random peer
 var passee_octet_iterator = Math.floor(Math.random() * 256);
-function pass_the_potato(potato){
-  var data = new Buffer(JSON.stringify(potato), 'utf8');
-  for(var i = 1; i !== 257; ++i){
-    var attempted_octet = (passee_octet_iterator + i) & 0xFF;
-    if(connections_by_octet[attempted_octet]){
-      // we found a socket to pass to
-      console.log('POTATO PASSED');
-      passee_octet_iterator = attempted_octet;
-      connections_by_octet[attempted_octet].write(data);
-      return;
+var mostRecent = "12:terribleplan";
+function pass_the_potato(potato) {
+    var temp = {};
+    try {
+        temp = bencode.decode(potato);
+    } catch (e) {
+        try {
+            temp = JSON.parse(potato);
+            mostRecent = bencode.encode(temp);
+        } catch (e) {
+            temp = {"message": "Some idiot sent me non-bEncode"};
+        }
     }
-  }
+    temp.via = ["terribleplan"];
 
-  // no one to pass to ...
-  console.log('UNABLE TO FIND ANYONE TO PASS THE POTATO TO :-(');
-  console.log('POTATO DROPPED');
+    var beerCount = 100;
+    if (temp["bottles_of_beer"]) {
+        parseInt(temp["bottles_of_beer"]);
+    } else if (temp.beer) {
+        parseInt(temp.beer);
+    }
+    beerCount--;
+    temp["bottles_of_beer"] = beerCount;
+    temp["beer"] = beerCount + " Bottles of beer on the wall "+beerCount+" bottles of beer, take one down and pass it around, "+(beerCount-1)+" bottles of beer on the wall";
+    mostRecent = bencode.encode(temp);
+
+    for(var i = 1; i !== 257; ++i){
+        var attempted_octet = (passee_octet_iterator + i) & 0xFF;
+        if(connections_by_octet[attempted_octet]){
+            // we found a socket to pass to
+            console.log('POTATO PASSED');
+            passee_octet_iterator = attempted_octet;
+            connections_by_octet[attempted_octet].write(mostRecent);
+            return;
+        }
+    }
+
+    // no one to pass to ...
+    console.log('UNABLE TO FIND ANYONE TO PASS THE POTATO TO :-(');
+    console.log('POTATO DROPPED');
 }
 
 // helper to initialize a socket connection
 function init_socket(socket){
-  var data = new Buffer(0);
-  var timeout = null;
+    var data = new Buffer(0);
+    var timeout = null;
 
-  // figure out the octet we are receiving this connection from
-  var octet = parseInt(socket.remoteAddress.split('.')[3], 10);
-  console.log('Connection established to host ' + LOCAL_NETWORK_PREFIX + octet);
+    // figure out the octet we are receiving this connection from
+    var octet = parseInt(socket.remoteAddress.split('.')[3], 10);
+    console.log('Connection established to host ' + LOCAL_NETWORK_PREFIX + octet);
 
-  function pass_potato(){
-    data = data.toString();
-    console.log('PASSING POTATO FROM ' + octet + ':', data);
-    data.replace('/ oonst/g', '');
-    data.replace('/ new tater/g', '');
-    var potato = {
-      fixed_by: 'Nick',
-      via: ['Nick']
-    };
-    try {
-      potato = JSON.parse(data);
-      if(!potato.via) potato.via = ['Nick'];
-      else potato.via.push('Nick');
-
-      if(potato.beer){
-        var m = potato.beer.parseInt(10);
-        var bottles_of_beer = m[1];
-        if(bottles_of_beer > 50) potato.beer = (bottles_of_beer) + " Bottles of beer on the wall " + (bottles_of_beer) + " bottles of beer, take 50 down and get really drunk, " + (bottles_of_beer - 50) + " bottles of beer on the wall";
-        else if(bottles_of_beer === 1) potato.beer = "NO MORE BEER!";
-        else potato.beer = (bottles_of_beer) + " Bottles of beer on the wall " + (bottles_of_beer) + " bottles of beer, take one down and pass it around, " + (bottles_of_beer - 1) + " bottles of beer on the wall";
-      }
-      else {
-        potato.beer = "10 Bottles of beer on the wall 10 bottles of beer, take one down and pass it around, 9 bottles of beer on the wall";
-      }
+    function pass_potato(){
+        console.log('PASSING POTATO FROM ' + octet + ':', data.toString());
+        pass_the_potato(data);
+        data = new Buffer(0);
+        timeout = null;
     }
-    catch(e){
+
+    function on_data(new_data){
+        data = Buffer.concat([data, new_data]);
+        if(timeout) clearTimeout(timeout);
+        timeout = setTimeout(pass_potato, 20);
     }
-    pass_the_potato(potato);
-    data = new Buffer(0);
-    timeout = null;
-  }
 
-  function on_data(new_data){
-    data = Buffer.concat([data, new_data]);
-    if(timeout) clearTimeout(timeout);
-    timeout = setTimeout(pass_potato, 20);
-  }
-
-  function on_close(){
-    socket.destroy();
-    if(connections_by_octet[octet] === socket){
-      console.log('Connection to host ' + LOCAL_NETWORK_PREFIX + octet + ' lost');
-      connections_by_octet[octet] = null;
+    function on_close(){
+        socket.destroy();
+        if(connections_by_octet[octet] === socket){
+            console.log('Connection to host ' + LOCAL_NETWORK_PREFIX + octet + ' lost');
+            connections_by_octet[octet] = null;
+        }
     }
-  }
 
-  if(connections_by_octet[octet]){
-    // if we already have a connection to that host, close this connection
-    socket.destroy();
-  }
-  else {
-    connections_by_octet[octet] = socket;
-    socket.on('end', on_close);
-    socket.on('close', on_close);
-    socket.on('error', on_close);
-    socket.on('data', on_data);
-  }
+    if(connections_by_octet[octet]){
+        // if we already have a connection to that host, close this connection
+        socket.destroy();
+    }
+    else {
+        connections_by_octet[octet] = socket;
+        socket.on('end', on_close);
+        socket.on('close', on_close);
+        socket.on('error', on_close);
+        socket.on('data', on_data);
+    }
 }
 
 function init_socket_this(){
-  init_socket(this);
+    init_socket(this);
 }
 
 // listen for connections
 net.createServer(init_socket).listen(LISTEN_PORT, function() {
-  console.log('Server listening on port ' + LISTEN_PORT);
+    console.log('Server listening on port ' + LISTEN_PORT);
 });
 
 // create connections
 function create_missing_connections(){
-  for(var octet = 1; octet !== 255; ++octet){
-    if(octet === 96) continue;
-    if(!connections_by_octet[octet]){
-      net.createConnection(LISTEN_PORT, LOCAL_NETWORK_PREFIX + octet, init_socket_this).on('error', function(){});
+    for(var octet = 1; octet !== 255; ++octet){
+        if(!connections_by_octet[octet]){
+            net.createConnection(LISTEN_PORT, LOCAL_NETWORK_PREFIX + octet, init_socket_this).on('error', function(){});
+        }
     }
-  }
 }
 
 create_missing_connections();
 setInterval(create_missing_connections, 45000);
 
 setTimeout(function(){
-  pass_the_potato({from: 'Nick', packet_id: Math.floor(Math.random() * 100000000)});
+    pass_the_potato(new Buffer("POTATO", 'utf8'));
 }, 1000);
-
